ce90dbed0f5f7551a3342aa658d9393ac4a2edf8
Antoine Augusti <antoine.augusti@developpement-durable.gouv.fr>
2018-10-06 17:14:12 +0200

    First meaningful commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Trainline GDPR
+
+## What's this?
+**Short:** generate a CSV file of trips you've booked through Trainline from your data you have requested thanks to the GDPR.
+
+**Long:** I use [Trainline Europe](https://www.trainline.eu) to book my train tickets since it was called Capitaine Train. Thanks to the GDPR law and especially the portability section, you can request your personal data.
+
+The Trainline Europe team will gladly send you a JSON file by email. The goal of this repository is to get a single CSV file to analyse your personal trips (trips that you've actually done - not booked, cancelled or those who have expired).
+
+## Installation
+```sh
+git clone git@github.com:AntoineAugusti/trainline-gdpr.git
+cd trainline-gdpr
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Usage
+```
+$ python main.py -h
+usage: main.py [-h] filepath
+
+positional arguments:
+  filepath    The full path to the Trainline JSON file
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
+
+Therefore, you can call the script like this: `python main.py /tmp/trainline.json`
+
+## Generated CSV
+See the file [data_sample.csv](data_sample.csv) to look at a sample file!
---
+++ main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import json
+from argparse import ArgumentParser
+from collections import defaultdict
+
+import pandas as pd
+
+parser = ArgumentParser()
+parser.add_argument('filepath', help='The full path to the Trainline JSON file')
+args = parser.parse_args()
+
+with open(args.filepath) as f:
+    data = json.load(f)
+
+res = defaultdict(list)
+for pnr in data['pnrs']:
+    if pnr['status'] == 'emitted':
+        for leg in pnr['legs']:
+            res['code'].append(pnr['code'])
+            res['booked_at'].append(pnr['booked_at'])
+
+            res['carrier'].append(leg['carrier'])
+            res['travel_class'].append(leg['travel_class'])
+            res['train_type'].append(leg['train']['type'])
+            res['train_number'].append(leg['train']['number'])
+            res['departure_date'].append(leg['departure_date'])
+            res['arrival_date'].append(leg['arrival_date'])
+            res['co2_emission'].append(leg['co2_emission'])
+
+            for direction in ['departure', 'arrival']:
+                for key in ['slug', 'latitude', 'longitude', 'name', 'country', 'public_id']:
+                    res[direction + '_' + key].append(leg[direction][key])
+
+
+df = pd.DataFrame(res, columns=res.keys())
+grouped = df.groupby(['departure_slug', 'arrival_slug']).size().rename('total_travel_for_leg')
+df = df.join(grouped, on=['departure_slug', 'arrival_slug'])
+df.to_csv('data.csv', index=False)
