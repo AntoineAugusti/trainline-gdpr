d76123255a9cee824fbe482af9e80933af610adb
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-08-21 14:55:37 +0200

    WIP add total price and distance computings

---
+++ haversine.py
@@ -0,0 +1,21 @@
+# vectorized haversine function
+
+import numpy as np
+
+def haversine(lat1, lon1, lat2, lon2, to_radians=True, earth_radius=6371):
+    """
+    slightly modified version: of http://stackoverflow.com/a/29546836/2901002
+
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees or in radians)
+
+    All (lat, lon) coordinates must have numeric dtypes and be of equal length.
+
+    """
+    if to_radians:
+        lat1, lon1, lat2, lon2 = np.radians([lat1, lon1, lat2, lon2])
+
+    a = np.sin((lat2-lat1)/2.0)**2 + \
+        np.cos(lat1) * np.cos(lat2) * np.sin((lon2-lon1)/2.0)**2
+
+    return earth_radius * 2 * np.arcsin(np.sqrt(a))
--- main.py
+++ main.py
@@ -4,17 +4,28 @@ from argparse import ArgumentParser
 from collections import defaultdict
 
 import pandas as pd
+import numpy as np
+from haversine import haversine
 
 parser = ArgumentParser()
 parser.add_argument('filepath', help='The full path to the Trainline JSON file')
+parser.add_argument('--first-name', help='Filter journeys with a specific traveler first name')
 args = parser.parse_args()
 
 with open(args.filepath) as f:
     data = json.load(f)
 
 res = defaultdict(list)
+totals = defaultdict(int)
 for pnr in data['pnrs']:
-    if pnr['status'] == 'emitted' and not pnr['cancelled']:
+    travelers_first_names = [t["first_name"] for t in pnr["travelers"]]
+    if (
+        pnr['status'] == 'emitted' and not pnr['cancelled'] and
+        (args.first_name is None or args.first_name in travelers_first_names)
+    ):
+        if pnr.get("price"):
+            totals["price"] += pnr["price"]["cents"]
+            totals["price_per_traveler"] += float(pnr["price"]["cents"]) / len(pnr["travelers"])
         for leg in pnr['legs']:
             res['code'].append(pnr['code'])
             res['booked_at'].append(pnr['booked_at'])
@@ -35,4 +46,19 @@ for pnr in data['pnrs']:
 df = pd.DataFrame(res, columns=res.keys())
 grouped = df.groupby(['departure_slug', 'arrival_slug']).size().rename('total_travel_for_leg')
 df = df.join(grouped, on=['departure_slug', 'arrival_slug'])
+df['distance_km'] = np.round(
+    haversine(
+        df.departure_latitude, df.departure_longitude,
+        df.arrival_latitude, df.arrival_longitude
+    ),
+    2
+)
 df.to_csv('data.csv', index=False)
+
+print("total price: %s EUR" % round(float(totals["price"]) / 100))
+print("total price per traveler: %s EUR" % round(float(totals["price_per_traveler"]) / 100))
+print("total distance: %s km" % round(np.sum(df.distance_km)))
+print(
+    "average price/km/traveler : %s EUR/km/passenger" %
+    round(float(totals["price_per_traveler"]) / 100.0 / np.sum(df.distance_km), 2)
+)
